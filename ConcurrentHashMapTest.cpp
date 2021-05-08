42ccf2c5645e8dcb7bf2a958ff865911cd12f6d2
Scorpio <642960662@qq.com>
2021-05-08 19:26:21 +0800

    add latency Test code

--- ConcurrentHashMapTest.cpp
+++ ConcurrentHashMapTest.cpp
@@ -1,4 +1,7 @@
 #include "ConcurrentHashMap.hpp"
+#include "Utils.hpp"
+
+#define N 100000000
 
 struct MarketData
 {
@@ -8,47 +11,71 @@ struct MarketData
     double ClosePrice;
 };
 
+double getdetlatimeofday(struct timeval *begin, struct timeval *end)
+{
+    return (end->tv_sec + end->tv_usec * 1.0 / 1000000) -
+           (begin->tv_sec + begin->tv_usec * 1.0 / 1000000);
+}
+
 int main(int ragc, char *argv[])
 {
     // producer
-    // std::unordered_map<std::string, int> tickerIndexMap;
-    // tickerIndexMap["SZ000001"] = 0;
-    // tickerIndexMap["SZ000002"] = 1;
-    // tickerIndexMap["SZ000003"] = 2;
-    // ConcurrentHashMap<MarketData> conMap(3, 0XFF000001, tickerIndexMap);
-
-    // MarketData market;
-    // strncpy(market.Ticker, "SZ000001", sizeof(market.Ticker));
-    // market.OpenPrice = 1.0;
-    // conMap.insert("SZ000001", market);
-
-    // strncpy(market.Ticker, "SZ000002", sizeof(market.Ticker));
-    // market.OpenPrice = 2.0;
-    // conMap.insert("SZ000002", market);
-
-    // strncpy(market.Ticker, "SZ000003", sizeof(market.Ticker));
-    // market.OpenPrice = 3.0;
-    // conMap.insert("SZ000003", market);
-
-    // consumer
     std::unordered_map<std::string, int> tickerIndexMap;
     tickerIndexMap["SZ000001"] = 0;
     tickerIndexMap["SZ000002"] = 1;
     tickerIndexMap["SZ000003"] = 2;
     ConcurrentHashMap<MarketData> conMap(3, 0XFF000001, tickerIndexMap);
 
-    MarketData market;
-    std::string ticker = "SZ000001";
-    conMap.get(ticker, market);
-    printf("Ticker %s OpenPrice %2.f\n", market.Ticker, market.OpenPrice);
+    struct timeval begin, end;
+    gettimeofday(&begin, NULL);
+    for (size_t i = 0; i < N; i++)
+    {
+        MarketData market;
+        strncpy(market.Ticker, "SZ000001", sizeof(market.Ticker));
+        market.OpenPrice = 1.0;
+        conMap.insert("SZ000001", market);
 
-    ticker = "SZ000002";
-    conMap.get(ticker, market);
-    printf("Ticker %s OpenPrice %2.f\n", market.Ticker, market.OpenPrice);
+        strncpy(market.Ticker, "SZ000002", sizeof(market.Ticker));
+        market.OpenPrice = 2.0;
+        conMap.insert("SZ000002", market);
 
-    ticker = "SZ000003";
-    conMap.get(ticker, market);
-    printf("Ticker %s OpenPrice %2.f\n", market.Ticker, market.OpenPrice);
+        strncpy(market.Ticker, "SZ000003", sizeof(market.Ticker));
+        market.OpenPrice = 3.0;
+        conMap.insert("SZ000003", market);
+    }
+
+    gettimeofday(&end, NULL);
+    double tm = getdetlatimeofday(&begin, &end);
+    printf("producer tid=%lu %f MB/s %f msg/s elapsed= %f size= %u\n", pthread_self(), 3 * N * sizeof(MarketData) * 1.0 / (tm * 1024 * 1024), 3 * N * 1.0 / tm, tm, 3 * N);
+
+    // consumer
+
+    // std::unordered_map<std::string, int> tickerIndexMap;
+    // tickerIndexMap["SZ000001"] = 0;
+    // tickerIndexMap["SZ000002"] = 1;
+    // tickerIndexMap["SZ000003"] = 2;
+    // ConcurrentHashMap<MarketData> conMap(3, 0XFF000001, tickerIndexMap);
+
+    // struct timeval begin, end;
+    // gettimeofday(&begin, NULL);
+    // for (size_t i = 0; i < N; i++)
+    // {
+    //     MarketData market;
+    //     std::string ticker = "SZ000001";
+    //     conMap.get(ticker, market);
+    //     // printf("Ticker %s OpenPrice %2.f\n", market.Ticker, market.OpenPrice);
+
+    //     ticker = "SZ000002";
+    //     conMap.get(ticker, market);
+    //     // printf("Ticker %s OpenPrice %2.f\n", market.Ticker, market.OpenPrice);
+
+    //     ticker = "SZ000003";
+    //     conMap.get(ticker, market);
+    //     // printf("Ticker %s OpenPrice %2.f\n", market.Ticker, market.OpenPrice);
+    // }
+    // gettimeofday(&end, NULL);
+    // double tm = getdetlatimeofday(&begin, &end);
+    // printf("consumer tid=%lu %f MB/s %f msg/s elapsed= %f size= %u\n", pthread_self(), 3 * N * sizeof(MarketData) * 1.0 / (tm * 1024 * 1024), 3 * N * 1.0 / tm, tm, 3 * N);
 
     // wait
     getchar();
